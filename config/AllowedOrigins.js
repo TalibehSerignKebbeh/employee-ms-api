f46c0bad0141d07d13e49c440020146d20a0c897
Modou Mbye <mm21821027@ut.edu.gm>
2024-04-04 07:50:04 +0000

    origins update

--- config/AllowedOrigins.js
+++ config/AllowedOrigins.js
@@ -2,7 +2,7 @@ const allowedOrigins = [
     'http://localhost:3000',
     'http://localhost:3000/users',
     'http://utg.gm/login',
-    'https://employee-ms-6cp9.onrender.com/',
+    'https://employee-ms-6cp9.onrender.com',
     'http://localhost:8081'
 ]
 
